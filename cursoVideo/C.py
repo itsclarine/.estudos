c00fee1e114becb7e3a66511a6ef8585dce26ca9
clara <studiessouza@gmail.com>
2026-04-04 16:01:29 -0300

    fix/organizando páginas

--- cursoVideo/C.py
+++
@@ -1,9 +0,0 @@
-#03: conversão de temperatura | F -> C 
-
-# algoritmo para converter temperatura  
-
-F = float(input('digite a temperatura em °F: ')) 
-
-C = (F - 32) / 1.8 
-
-print(f'a temperatura em Celsius  é de {C:.2f}') 
