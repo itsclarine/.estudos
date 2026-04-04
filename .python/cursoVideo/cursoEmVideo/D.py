46a815060933e9497b62cbb8138633a389990d60
clara <studiessouza@gmail.com>
2026-04-04 16:13:37 -0300

    fix/organizando

--- .python/cursoVideo/cursoEmVideo/D.py
+++
@@ -1,8 +0,0 @@
-# 04: taxação | 60%  
-# algoritmo que calcula taxação sob o valor do produto 
-
-Preco_produto = float(input('digite o valor do produto: ')) 
-
-Novo_preco = (Preco_produto * 60) / 100  
-
-print(f'você pagará {Novo_preco:.2f} de imposto') 
