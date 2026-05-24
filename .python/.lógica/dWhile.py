2c7236612cd9bd019ba696d3265fc4ef45e7aff0
clara <studiessouza@gmail.com>
2026-05-24 11:31:10 -0300

    update exercicio

---
+++ .python/.lógica/dWhile.py
@@ -0,0 +1,15 @@
+# ====contagem inteligente 
+
+print('CONTAGEM INTELIGENTE')
+print('---------------------')
+
+contagem_I = int(input('Início: '))
+contagem_F = int(input('Fim: '))
+
+print('---------------------')
+print(' C O N T A N D O ')
+print('---------------------')
+
+if contagem_I < contagem_F:
+  while 
+    
